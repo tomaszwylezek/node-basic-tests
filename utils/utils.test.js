8171573677f1df0fd509ac556103a36b03ad821d
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-26 14:19:52 +0100

    Add assertion library and some test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,19 +1,39 @@
-const utils = require('./utils');
+const expect = require("expect");
 
-it('should add two number', () => {
-  const res = utils.add(33,11);
+const utils = require("./utils");
 
-  if(res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
+it("should add two number", () => {
+  const res = utils.add(33, 11);
 
-  }
+  expect(res)
+    .toBe(44)
+    .toBeA("number");
 });
 
-it('square number', () => {
+it("should square number", () => {
   const res = utils.square(10);
 
-  if(res !== 100) {
-    throw new Error(`Expected 100, but got ${res}`);
+  expect(res)
+    .toBe(100)
+    .toBeA("number");
+});
 
-  }
+it("should verify first and last names are set", () => {
+  const expected = {
+    age: 12,
+    firstName: "John",
+    lastName: "Doe"
+  };
+
+  const mock = {
+    age: 12
+  };
+
+  const user = utils.setName(mock, "John Doe");
+
+  expect(user).toEqual(expected);
+  expect(user).toInclude({
+    firstName: "John",
+    lastName: "Doe"
+  });
 });
